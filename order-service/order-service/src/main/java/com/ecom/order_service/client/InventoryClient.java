0e4d10e90b3bbcad673dde4966c3f4f4364a5ee7
Dinesh Rajbhar <dineshrajbhar9595066@gmail.com>
2026-07-15 11:41:14 +0530

    Implemented API Gateway and Eureka service discovery

--- order-service/order-service/src/main/java/com/ecom/order_service/client/InventoryClient.java
+++ order-service/order-service/src/main/java/com/ecom/order_service/client/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8081")
+@FeignClient(name = "inventory-service")
 public interface InventoryClient {
 
     @GetMapping("/api/inventory/{productId}")
